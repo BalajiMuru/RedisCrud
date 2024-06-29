c947919075808da5cd2e26689baae5c2dfb511fb
mahesh.k_grt <mahesh.k@grtship.com>
2024-06-29 18:33:28 +0530

    REDIS

---
+++ src/main/java/com/api/radis/RadisApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.radis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadisApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/radis/RadisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.radis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
